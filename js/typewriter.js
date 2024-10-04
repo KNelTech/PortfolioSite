1c77df3fbcba32b67a03f44d9cd4f7c1c0bf12a2
footGunDev <83507106+KNelTech@users.noreply.github.com>
2024-10-04 16:36:31 -0500

    main layout done. next is mobile responsiveness

--- js/typewriter.js
+++ js/typewriter.js
@@ -12,39 +12,39 @@ document.addEventListener("DOMContentLoaded", function () {
       .typeString(
         `
 param (
-  [string]$DriverName = "Totally Not Malicious",
-  [string]$NewUser = "",
-  [string]$NewPassword = "",
-  [string]$DLL = ""
-  )
-  
+[string]$DriverName = "Totally Not Malicious",
+[string]$NewUser = "",
+[string]$NewPassword = "",
+[string]$DLL = ""
+)
+
 if ( $DLL -eq "" ){
-  $nightmare_data = [byte[]](get_nightmare_dll)
-  $encoder = New-Object System.Text.UnicodeEncoding
+$nightmare_data = [byte[]](get_nightmare_dll)
+$encoder = New-Object System.Text.UnicodeEncoding
 
-  if ( $NewUser -ne "" ) {
-    $NewUserBytes = $encoder.GetBytes($NewUser)
-    [System.Buffer]::BlockCopy($NewUserBytes, 0, 
-    $nightmare_data[0x32e20+$NewUserBytes.Length] = 0
-    $nightmare_data[0x32e20+$NewUserBytes.Length+1] = 0
-    } else {
-      Write-Host "[+] using default new user: adm1n"
-    }
-
-  if ( $NewPassword -ne "" ) {
-     $NewPasswordBytes = $encoder.GetBytes($NewPassword)
-     [System.Buffer]::BlockCopy($NewPasswordBytes, 0, 
-     $nightmare_data[0x32c20+$NewPasswordBytes.Length+1] = 0
-    } else {
-     Write-Host "[+] using default new password: P@ssw0rd"
-    }
-
-    $DLL = [System.IO.Path]::GetTempPath() + "nightmare.dll"
-    [System.IO.File]::WriteAllBytes($DLL, $nightmare_data)
-    Write-Host "[+] created payload at $DLL"
-    $delete_me = $true
+if ( $NewUser -ne "" ) {
+  $NewUserBytes = $encoder.GetBytes($NewUser)
+  [System.Buffer]::BlockCopy($NewUserBytes, 0, 
+  $nightmare_data[0x32e20+$NewUserBytes.Length] = 0
+  $nightmare_data[0x32e20+$NewUserBytes.Length+1] = 0
   } else {
-      Write-Host "[+] using user-supplied payload at $DLL" `
+    Write-Host "[+] using default new user: adm1n"
+  }
+
+if ( $NewPassword -ne "" ) {
+    $NewPasswordBytes = $encoder.GetBytes($NewPassword)
+    [System.Buffer]::BlockCopy($NewPasswordBytes, 0, 
+    $nightmare_data[0x32c20+$NewPasswordBytes.Length+1] = 0
+  } else {
+    Write-Host "[+] using default new password: P@ssw0rd"
+  }
+
+  $DLL = [System.IO.Path]::GetTempPath() + "nightmare.dll"
+  [System.IO.File]::WriteAllBytes($DLL, $nightmare_data)
+  Write-Host "[+] created payload at $DLL"
+  $delete_me = $true
+} else {
+    Write-Host "[+] using user-supplied payload at $DLL" `
       )
       .pauseFor(1000)
       .start();
